d4580fe4395582f4e85606675d97209f124d6916
cvjuan270 <cvjuan270@gmail.com>
2022-07-20 17:06:20 -0500

    armando json

--- __manifest__.py
+++ __manifest__.py
@@ -1,35 +1,35 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "ef_sfs",
+    'name': "Facturacion electronica con SFS_1.5",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    Genera documentos electronicos a travez de la API del SFS_1.5 SUNAT""",
 
     'description': """
-        Long description of module's purpose
+        Emite Facturas electronica Peru a travez del API SFS_1.5 SUNAT
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Juan Collado",
+    'website': "cvjuan@gmail.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Accounting',
     'version': '0.1',
+    'license': 'AGPL-3',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','account'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        #'views/views.xml',
+        'views/account_move_views.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
+        #'demo/demo.xml',
     ],
 }
