5fc22e480ea89e8dffe762e7f0c5162fd1d3beff
cvjuan270 <cvjuan270@gmail.com>
2022-08-16 13:55:38 -0500

    add sequece hack

--- static/ejercicios.py
+++ static/ejercicios.py
@@ -1,11 +1,16 @@
 def run():
-    sfs_id_situ = '01'
 
-    if sfs_id_situ not in ('03','05'):
-        print(':)')
-    else:
-        print(':(')
+    lst_tareas = ['paso1', 'paso 2', 'paso 3']
 
+    lst_tareas.append('paso 4')
+
+    print(lst_tareas[2])
+
+    tupla_tareas = ('paso1', 'paso 2', 'paso n')
+
+    dicc_tareas = {'steep1':'comprar insumos', 'steep2':'Almacenarlos', 'steep3':'preparar'}
+
+    print(dicc_tareas['steep5'])
 
 if __name__ == "__main__":
     run()
