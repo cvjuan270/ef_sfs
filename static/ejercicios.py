ed4f352ae1150410290bb564267e0f2dab67ffd7
cvjuan270 <cvjuan270@gmail.com>
2022-09-09 15:40:44 -0500

    Correccion calculo de igv y tipo producto

--- static/ejercicios.py
+++ static/ejercicios.py
@@ -6,11 +6,6 @@ def run():
 
     print(lst_tareas[2])
 
-    tupla_tareas = ('paso1', 'paso 2', 'paso n')
-
-    dicc_tareas = {'steep1':'comprar insumos', 'steep2':'Almacenarlos', 'steep3':'preparar'}
-
-    print(dicc_tareas['steep5'])
 
 if __name__ == "__main__":
     run()
